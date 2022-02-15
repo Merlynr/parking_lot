53f2050a361ebba49bd75fe0f574a64cc238907e
merlynr <lcq1013962426@gmail.com>
2022-02-15 22:51:22 +0800

    解决HTTPS的访问

--- parking-wx/dist/pages/home/index.js
+++ parking-wx/dist/pages/home/index.js
@@ -11,21 +11,31 @@ exports.default = Page({
     four: "",
     five: "",
     six: "",
-    seven: ""
+    seven: "",
+    parkingRecord: ""
   },
   submit: function submit() {
+    var that = this;
     var str = this.data.one + this.data.two + this.data.three + this.data.four + this.data.five + this.data.six + this.data.seven;
     if (str.length == 7) {
-      wx.showToast({
-        title: '成功',
-        icon: 'succes',
-        duration: 1000,
-        mask: true
+      wx.request({
+        url: "http://127.0.0.1:8848/api/TODO",
+        success: function success(res) {
+          wx.showToast({
+            title: "成功",
+            icon: "success",
+            duration: 1000,
+            mask: true
+          });
+          that.setData({
+            parkingRecord: res.data
+          });
+        }
       });
     } else {
       wx.showToast({
-        title: '未填写完整',
-        icon: 'error',
+        title: "未填写完整",
+        icon: "error",
         duration: 1000,
         mask: true
       });
