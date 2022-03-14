616f6d97e47cd8d928d3f6d22b2b7068347a7c11
merlynr <lcq1013962426@gmail.com>
2022-03-14 19:58:51 +0800

    第二页

--- parking-wx/dist/pages/money/money.js
+++ parking-wx/dist/pages/money/money.js
@@ -5,13 +5,33 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = Page({
   data: {
-    license: "",
-    start: "",
-    end: "",
+    license: "鲁JRW350",
+    start: "2022-03-04 14:31:15",
+    end: "2022-03-09 15:17:25",
     time: "",
-    money: ""
+    money: "",
+    queryBean: {}
+  },
+  onLoad: function onLoad(options) {
+    var that = this;
+    var queryBean = JSON.parse(options.queryBean);
+    that.setData({
+      queryBean: queryBean
+    });
+    console.log(that.data.queryBean);
   },
   submit: function submit() {
-    console.log("sss");
+    var that = this;
+    wx.request({
+      method: "POST",
+      url: "http://127.0.0.1:8848/api/parking/update",
+      header: {
+        token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxIn0.qfd0G-elhE1aGr15LrnYlIZ_3UToaOM5HeMcXrmDGBM"
+      },
+      data: {
+        id: that.data.queryBean.id,
+        money: that.data.queryBean.money
+      }
+    });
   }
 });
